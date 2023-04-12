7ec686ddbd930296b4944409ff7a4d28bd84c943
Antony Paquay <apaquay@worldofdigits.com>
2023-04-12 22:11:37 +0200

    feat(): return logs when an error is occured

--- gulpfile.js
+++ gulpfile.js
@@ -11,64 +11,78 @@ const babel = require('rollup-plugin-babel');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const uglify = require('gulp-uglify');
+const notify = require('gulp-notify');
 
 const paths = {
-    styles: {
-        src: "src/scss/styles.scss",
-        watch: "src/scss/**/*.scss",
-        dest: "public/css"
-    },
-    scripts: {
-        src: "src/js/app.js",
-        watch: "src/js/**/*.js",
-        dest: "public/js"
-    },
-    templates: {
-        src: "public/**/*.html",
-        watch: "public/**/*.html"
-    }
+  styles: {
+    src: "src/scss/styles.scss",
+    watch: "src/scss/**/*.scss",
+    dest: "public/css"
+  },
+  scripts: {
+    src: "src/js/app.js",
+    watch: "src/js/**/*.js",
+    dest: "public/js"
+  },
+  templates: {
+    src: "public/**/*.html",
+    watch: "public/**/*.html"
+  }
 };
 
 function styles() {
-    return gulp
-        .src(paths.styles.src)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(paths.styles.dest))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass()
+      .on("error", function (err) {
+        notify({
+          title: "SASS Error: " + err.code,
+          message: err.message,
+          sound: false
+        }).write(err);
+        return this.emit("end");
+      }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
-    return gulp.src(paths.scripts.src)
-        .pipe(sourcemaps.init())
-        .pipe(rollup({
-            plugins: [
-                babel({presets: ['@babel/env']}), resolve(), commonjs()
-            ]
-        }, 'umd'))
-        .pipe(uglify())
-        .pipe(rename("bundle.min.js"))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(browserSync.reload({stream: true}));
+  return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(rollup({ plugins: [babel({ presets: ['@babel/env'] }), resolve(), commonjs()] }, 'umd')
+      .on("error", function (err) {
+        notify({
+          title: "JS Error: " + err.code,
+          message: err.message,
+          sound: false
+        }).write(err);
+        return this.emit("end");
+      })
+    )
+    .pipe(uglify())
+    .pipe(rename("bundle.min.js"))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 function reload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
-    gulp.watch(paths.styles.watch, styles);
-    gulp.watch(paths.scripts.watch, scripts);
-    gulp.watch(paths.templates.watch, reload);
+  browserSync.init({
+    server: {
+      baseDir: "./public"
+    }
+  });
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.scripts.watch, scripts);
+  gulp.watch(paths.templates.watch, reload);
 }
 
 gulp.task("default", watch);
